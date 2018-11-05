63223f4d7d34d5488eeece52f08251513522a4a4
sys <suyonsohn@gmail.com>
2018-11-04 16:06:43 -0800

    Give write access to peer

--- orbitdb.js
+++ orbitdb.js
@@ -17,13 +17,24 @@ const ipfs = new IPFS(ipfsOptions)
 ipfs.on('ready', async () => {
     // Create OrbitDB instance
     const orbitdb = new OrbitDB(ipfs)
-    const db = await orbitdb.keyvalue('word-database')
-    console.log(db.address.toString())
+
+    const access = {
+        // Give write access to ourselves
+        write: [
+            orbitdb.key.getPublic('hex'),
+            // Give access to peer
+            // '04b2071b3d2e2a0cfecc47b12b765165576bdd6dc441bca71e279e5a7ffbc2ba2f5b554bb5e7a111092430611148ec39a11fcf999810b04be069d151776ca55e0d'
+        ],
+    }
+
+    const db1 = await orbitdb.keyvalue('word-database', access)
+    console.log(`DB ADDRESS: ${db1.address.toString()}`)
     // /orbitdb/QmPjF3u91uQdLrSBY2TxawPbUnTx8kahQEUAYK1SYJhkmi/word-database
+
+    // Second peer opens the database from the address
+    const db2 = await orbitdb.keyvalue(db1.address.toString())
 
     const key = db.key
     // Public key is required to grant write access
-    console.log(db.key.getPublic('hex'))
-
-
+    console.log(`If I give my public key to you, you have to let me write to your db: ${db.key.getPublic('hex')}`)
 })
